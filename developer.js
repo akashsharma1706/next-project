83de13d0b36e063fe0cf0ab84b1bec25b5e891ba
akashsharma1706 <131576871+akashsharma1706@users.noreply.github.com>
2024-04-19 21:42:50 +0530

    Add files via upload

---
+++ developer.js
@@ -0,0 +1,15 @@
+// pages/aboutus/developer.js
+
+import React from 'react';
+
+const DeveloperPage = () => {
+    return (
+        <div>
+            <h1>Developer Page</h1>
+            <p>This page is under the About Us section.</p>
+            <p>Created by: Akash Sharma</p> {/* Your name here */}
+        </div>
+    );
+};
+
+export default DeveloperPage;
