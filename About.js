7e83523d00f52cf2e19ebcc5b969640a002cf025
akashsharma1706 <131576871+akashsharma1706@users.noreply.github.com>
2024-04-19 21:40:16 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,14 @@
+// pages/aboutus.js
+
+import React from 'react';
+
+const AboutUs = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Welcome to our company's About Us page!</p>
+        </div>
+    );
+};
+
+export default AboutUs;
